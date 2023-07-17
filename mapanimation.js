4ccb9d78e4834897a52b6a887244d7797fecebaf
Will Wright <willwright@Wills-MacBook-Pro-2.local>
2023-07-17 11:41:41 -0400

    added another button

--- mapanimation.js
+++ mapanimation.js
@@ -1,10 +1,13 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoid3dyaWdodDIxIiwiYSI6ImNsandzMno0ZDA1aTAzZm15bG1nZzZlcWMifQ.Ul9Oy40d-hWa_hvwaLNXbw';
 
+const initialCenter = [-71.096679, 42.360019]
+const initialZoom = 12.5
+
 var map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/dark-v11',
-	center: [-71.089023, 42.350578],
-	zoom: 14,
+	center: initialCenter,
+	zoom: initialZoom,
 	// accessToken: mapboxToken
 });
 
@@ -19,16 +22,33 @@ document.querySelector('#fly-to-MIT').addEventListener('click', () => {
 	})
 })
 
-// reset the map view
-document.querySelector('#reset-view').addEventListener('click', () => {
+// fly to Harvard Campus
+document.querySelector('#fly-to-Harvard').addEventListener('click', () => {
 	map.flyTo({
-		center: [-71.089023, 42.350578],
-		zoom: 14,
+		center: [-71.116808, 42.378102],
+		zoom: 16,
 	})
 })
 
-// create bus stops
+// reset the map view
+document.querySelector('#reset-view').addEventListener('click', () => {
+	map.flyTo({
+		center: initialCenter,
+		zoom: initialZoom,
+	})
+})
 
+//  // toggle bus Stops
+//  document.querySelector('#toggle-stops').addEventListener('click', () => {
+// 	const isVisible = map.getLayoutProperty('busStops', 'visibility') === 'visible'
+// 	if (isVisible) {
+// 		map.setLayoutProperty('busStops', 'visibility', 'none')
+// 	} else {
+// 		map.setLayoutProperty('busStops', 'visibility', 'visible')
+// 	}
+// })
+
+// create bus stops
 const busStops = [
 	[-71.093729, 42.359244],
 	[-71.094915, 42.360175],
@@ -44,8 +64,40 @@ const busStops = [
 	[-71.118625, 42.374863]
 ];
 
+// // the new way
+// // Create a GeoJSON feature collection for bus stops
+// const busStopFeatures = busStops.map(busStop => ({
+// 	type: 'Feature',
+// 	properties: {},
+// 	geometry: {
+// 	  type: 'Point',
+// 	  coordinates: busStop
+// 	}
+//   }));
+  
+//   // Add bus stop markers as a map layer
+//   map.on('load', function() {
+// 	map.addLayer({
+// 	  id: 'busStopsLayer',
+// 	  type: 'symbol',
+// 	  source: {
+// 		type: 'geojson',
+// 		data: {
+// 		  type: 'FeatureCollection',
+// 		  features: busStopFeatures
+// 		}
+// 	  },
+// 	  layout: {
+// 		'icon-image': 'marker-15',
+// 		'icon-size': 1.5
+// 	  },
+// 	  paint: {}
+// 	});
+//   });
+
+
+// the old way
 busStops.forEach(busStop => {
-	const { latitude, longitude} = busStop;
 	const stopLoc = new mapboxgl.Marker({
 		color: '#ff0000',
 		scale: 0.90
@@ -54,12 +106,12 @@ busStops.forEach(busStop => {
 		.addTo(map);
 
 	const stopPopup = new mapboxgl.Popup({ offset: 32 })
-	.setHTML(`<b>Bus Stop lat/long:</b><br> ${busStop[1]}, ${busStop[0]}`)
+		.setHTML(`<b>Bus Stop lat/long:</b><br> ${busStop[1]}, ${busStop[0]}`)
 
 	stopLoc.setPopup(stopPopup);
 });
 
-
+// get bus locations from the API
 async function run() {
 	const locations = await getBusLocations();
 	// get bus data    
